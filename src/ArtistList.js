ecaea65602a38ff56d26aba100b74d5decf5bade
AlejandroFuentes14 <jfuentes0@ucol.mx>
2023-11-19 12:05:09 -0600

    Listo

--- src/ArtistList.js
+++ src/ArtistList.js
@@ -2,58 +2,54 @@ import React, { Component } from "react";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { Actions } from "react-native-router-flux";
 
-import ListView from "deprecated-react-native-listview";
+import { FlatList } from "react-native"; // Replace deprecated ListView with FlatList
 import ArtistBox from "./ArtistBox";
 
 export default class ArtistList extends Component {
   constructor(props) {
     super(props);
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2,
-    });
-
     this.state = {
-      dataSource: ds.cloneWithRows(props.artists || []),
+      dataSource: this.prepareDataSource(props.artists || []),
     };
   }
 
-  updateDataSource = (data) => {
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(data || []),
-    });
+  prepareDataSource = (data) => {
+    return data.map((item, index) => ({
+      ...item,
+      key: `${index}`, // Assign a unique key for FlatList
+    }));
   };
-  
-  handlePress(artist) {
+
+  handlePress = (artist) => {
     if (artist && artist.id) {
       Actions.artistDetail({ artist });
     } else {
       console.error("No se pudo encontrar información del artista");
     }
+  };
+
+  renderArtist = ({ item }) => (
+    <TouchableOpacity onPress={() => this.handlePress(item)}>
+      <ArtistBox artist={item} />
+    </TouchableOpacity>
+  );
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.artists !== this.props.artists) {
+      this.setState({
+        dataSource: this.prepareDataSource(this.props.artists),
+      });
+    }
   }
 
   render() {
     return (
-      <ListView
+      <FlatList
+        data={this.state.dataSource}
+        renderItem={this.renderArtist}
+        keyExtractor={(item) => item.key}
         enableEmptySections={true}
-        dataSource={this.state.dataSource}
-        renderRow={(artist) => {
-          return (
-            <TouchableOpacity onPress={() => this.handlePress(artist)}>
-              <ArtistBox artist={artist} />
-            </TouchableOpacity>
-          );
-        }}
       />
     );
-  }
-
-  componentDidMount() {
-    this.updateDataSource(this.props.artists);
-  }
-
-  componentWillReceiveProps(newProps) {
-    if (newProps.artists !== this.props.artists) {
-      this.updateDataSource(newProps.artists);
-    }
   }
 }
